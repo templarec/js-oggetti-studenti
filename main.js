ce8bb681992b017b6ecf425aa1c4db1fc42b2c65
Lorenzo Bernini <templarec@gmail.com>
2021-03-16 16:00:04 +0100

    Esercizio completato

--- main.js
+++ main.js
@@ -17,37 +17,41 @@ $(document).ready(function () {
 	for (var studenteKey in studente) {
 		console.log(studenteKey, ":", studente[studenteKey]);
 	}
-	//creo array classe e lo popolo con vari studenti:
 	console.log("---------------");
+	//creo array classe e lo popolo con vari studenti:
 	var classe = [];
 	classe.push(studente);
 	//console.log("Stampo array: ", classe);
 	var sep = "</td><td>";
+	//stampo in tabella il primo nome in array creato dal codice sopra
 	output.append("<thead><th>Numero</th><th>Nome</th><th>Cognome</th><th>Età</th></thead>");
 	output.append("<tr><td>" + 1 + sep + classe[0].nome + sep + classe[0].cognome + sep + classe[0].eta + "</td></tr>");
-	//al click inserisco nuovo utente in array come oggetto
+	//al click inserisco nuovo utente in array come oggetto e stampo in tabella
 	button.click(function () {
 		//prendo le value degli input
 		var newNome = nome.val();
 		var newCognome = cognome.val();
 		var newEta = eta.val();
-		//creo oggetto studente
-		var studente = {
-			nome: newNome,
-			cognome: newCognome,
-			eta: newEta
+		//verifico che non siano vuote
+		if (newNome != "" && newCognome != "" && newEta != "" ) {
+			//creo oggetto studente
+			var studente = {
+				nome: newNome,
+				cognome: newCognome,
+				eta: newEta
+			}
+			//push dentro array classe
+			classe.push(studente);
+			//console.log("Stampo array aggiornato: ", classe);
+			//visualizzo elementi dom nascosti
+			$('#lista').show();
+			$('.output').show();
+			//appendo ultimo studente inserito che è l'ultimo in array
+			var ultimo = classe.length - 1;
+			output.append("<tr><td>" + (ultimo+1) + sep + classe[ultimo].nome + sep + classe[ultimo].cognome + sep + classe[ultimo].eta + "</td></tr>");
+		} else {
+			//avviso che non sono stati inseriti campi
+			alert("Attenzione i campi sono obbligatori!");
 		}
-		//push dentro array classe
-		classe.push(studente);
-		console.log("Stampo array aggiornato: ", classe);
-		//stampo ogni elemento array e oggetto
-		$('#lista').show();
-		$('.output').show();
-
-		var ultimo = classe.length - 1;
-		output.append("<tr><td>" + (ultimo+1) + sep + classe[ultimo].nome + sep + classe[ultimo].cognome + sep + classe[ultimo].eta + "</td></tr>");
-
-
 	})
-
 });
